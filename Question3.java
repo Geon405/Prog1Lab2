08a1345c9adb6d6b8a0a9ff72f9248aa87d26567
Geon405 <kg000405@gmail.com>
2020-02-15 14:21:44 -0500

    Edit the Question3

--- Question3.java
+++ Question3.java
@@ -1,20 +1,30 @@
 import java.util.Scanner;
 
+/**
+ * program the sum of the digits of integer.
+ * @author geonkim
+ */
 public class Question3 {
 
+    /**
+     * 
+     * @param args
+     */
     public static void main(String[] args){
 
         Scanner input = new Scanner(System.in);
 
+        // Prompt the user to enter 3 digits of integer
         System.out.print("Enter the 3 digits of integer: ");
         String number = input.nextLine();
         
-        char n1 = number.charAt(0);
+        char first = number.charAt(0);
         
-        char n2 = number.charAt(1);
+        char second = number.charAt(1);
         
-        char n3 = number.charAt(2);
+        char third = number.charAt(2);
         
-        System.out.println("Sum of 3 digits are " + (n1 + n2 + n3));        
+        // Sum of the 3 digits
+        System.out.println("Sum of 3 digits are " + ((first + second + third)-144));        
     }
 }
