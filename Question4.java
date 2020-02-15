1021b73389f71248b4931bab3ce9c035d3c41be9
Geon405 <kg000405@gmail.com>
2020-02-15 14:22:17 -0500

    Edit the Question4

--- Question4.java
+++ Question4.java
@@ -1,26 +1,39 @@
 import java.util.Scanner;
 
+/**
+ * program to check the URL is valid or not.
+ * @author geonkim
+ */
 public class Question4{
 
-	public static void main(String[] args){
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args){
             
-            // Prompt the user for a URL
-            System.out.print("Enter an email address: ");
+        // Prompt the user for a URL
+        System.out.print("Enter an email address: ");
 
-            Scanner in = new Scanner(System.in);
-            String email_address = in.nextLine();
+        Scanner in = new Scanner(System.in);
+        String email_address = in.nextLine();
             
-            System.out.println("Verifying an URL with X characters");
+        //To calculate the size of the URL
+        int count = 0;
             
-            // To find the position of "@"
-            int position = email_address.indexOf("@");
-
-            // To output the username
-            String user_name = email_address.substring(0, position);
-	    System.out.println("Username   : " + user_name);
-
-            // To output the domain name
-            String domain_name = email_address.substring(position+1);
-            System.out.println("Domain name: " + domain_name);
-	}
+        for(int i = 0; i<email_address.length(); i++){
+            if(email_address.charAt(i) != 0)
+                count++;
+        }
+        
+        System.out.println("Verifying an URL with " + count + " characters");
+        
+        boolean isValid = true;
+        
+        if(isValid){
+            System.out.println("Is this a valid and secure URL?: true");
+        } else {
+            System.out.println("Is this a valid and secure URL?: false");
+        }                    
+    }
 }
