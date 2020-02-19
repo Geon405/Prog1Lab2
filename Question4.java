d66fa6de3b0a55020e167dfea83913a3d8f501ad
Geon405 <kg000405@gmail.com>
2020-02-18 20:32:06 -0500

    Question4

--- Question4.java
+++ Question4.java
@@ -1,39 +1,27 @@
 import java.util.Scanner;
 
-/**
- * program to check the URL is valid or not.
- * @author geonkim
- */
 public class Question4{
 
-    /**
-     *
-     * @param args
-     */
     public static void main(String[] args){
-            
-        // Prompt the user for a URL
+
         Scanner kb = new Scanner(System.in);
-        System.out.print("Enter a URL: ");
-        String address = kb.nextLine();
-        int count = 0;
+
+        // Prompts the user to enter the temperature of past 3 days.
+        System.out.print("The temperature of past 3 days is ");
+        double d1 = kb.nextDouble();
             
-        for(int i = 0; i<address.length(); i++){
-            if(address.charAt(i) != 0)
-                count++;
-        }
-        
-        System.out.println("Verifying an URL with " + count + " characters");
-        int space = address.length();
-        String str = "";
-        String address2 = address.substring(0,8);
-        
-        if (address2.equals("https://")) {
-            System.out.println("Is this a valid and secure URL?: True");
-        }
-        else
-        {
-            System.out.println("Is this a valid and secure URL?: False");
-        }                    
+        // Prompts the user to enter the temperature of past 2 days.
+        System.out.print("The temperature of past 2 days is ");
+        double d2 = kb.nextDouble();
+
+        // Prompts the user to enter the temperature of past 1 day.
+        System.out.print("The temperature of past 1 days is ");
+        double d3 = kb.nextDouble();
+
+        // To calculates the average.
+        double average = ((d1+d2+d3)/3);
+
+        // Prints the average of temperature
+        System.out.printf("The average of temperature is %.2f C\n", average);
     }
 }
