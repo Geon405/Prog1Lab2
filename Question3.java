5ecb97a7b713e108999f81b89ac1bd0482b79d1f
Geon405 <kg000405@gmail.com>
2020-02-18 20:31:44 -0500

    Question3

--- Question3.java
+++ Question3.java
@@ -1,30 +1,21 @@
 import java.util.Scanner;
 
-/**
- * program the sum of the digits of integer.
- * @author geonkim
- */
-public class Question3 {
+public class Question3{
 
-    /**
-     * 
-     * @param args
-     */
-    public static void main(String[] args){
+public static void main(String[] args){
 
-        Scanner input = new Scanner(System.in);
+	Scanner kb = new Scanner(System.in);
 
-        // Prompt the user to enter 3 digits of integer
-        System.out.print("Enter the 3 digits of integer: ");
-        String number = input.nextLine();
+	System.out.print("Enter the amount of seconds: ");
+	int second = kb.nextInt();
+
+        // To convert second to minute.
+	int minutes = second/60;
         
-        char first = number.charAt(0);
-        
-        char second = number.charAt(1);
-        
-        char third = number.charAt(2);
-        
-        // Sum of the 3 digits
-        System.out.println("Sum of 3 digits are " + ((first + second + third)-144));        
-    }
+	int remainingSeconds = second % 60;
+
+        // Prints the output of time in minutes.
+	System.out.println("Convert seconds to minutes: " + minutes + ":" + remainingSeconds + " min");
+	}
 }
+
